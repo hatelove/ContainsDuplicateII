ded4ed6763d724af03b4fb8acd92525ab1e566a5
hatelove <jay.hatelove@gmail.com>
2017-02-07 15:31:31 +0800

    rename numbers to nums

--- ContainsDuplicateII/UnitTest1.cs
+++ ContainsDuplicateII/UnitTest1.cs
@@ -9,9 +9,9 @@ namespace ContainsDuplicateII
         [TestMethod]
         public void Test_k_is_zero_should_return_false()
         {
-            var numbers = new int[] { 1, 2, 3, 1, 2 };
+            var nums = new int[] { 1, 2, 3, 1, 2 };
             var k = 0;
-            Assert.IsFalse(Solution.ContainsNearbyDuplicate(numbers, k));
+            Assert.IsFalse(Solution.ContainsNearbyDuplicate(nums, k));
         }
     }
 
