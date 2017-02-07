29d66e73a3acc0bbc30a0c70daa94cad605bf56b
hatelove <jay.hatelove@gmail.com>
2017-02-07 15:27:44 +0800

    first test case failed: k = 0, should return false

---
+++ ContainsDuplicateII/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ContainsDuplicateII
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_distance_is_zero_should_return_false()
+        {
+            var numbers = new int[] { 1, 2, 3, 1, 2 };
+            var k = 0;
+            Assert.IsFalse(Solution.ContainsNearbyDuplicate(numbers, k));
+        }
+    }
+
+    public static class Solution
+    {
+        public static bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
